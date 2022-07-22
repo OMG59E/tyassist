858bdefb1b96f8157129d9a642ac01c67b6e8c55
邢卫国 <xing.weiguo@intellif.com>
2022-07-22 11:40:42 +0800

    :sparkles: 新增支持ILSVRC2015_val

--- datasets/imagenet.py
+++ datasets/imagenet.py
@@ -14,7 +14,7 @@ from base.dataset_base import DatasetBase
 
 class ILSVRC2012(DatasetBase):
     """
-    imagenet dataset
+    ILSVRC2012 val
     """
     def __init__(self, root_path, batch_size=1):
         """
@@ -67,3 +67,39 @@ class ILSVRC2012(DatasetBase):
         img_paths = self._filepaths[0:num]
         labels = self._labels[0:num]
         return img_paths, labels
+
+
+class ILSVRC2015(ILSVRC2012):
+    """
+    ILSVRC2015 val
+    """
+    def __init__(self, root_path, batch_size=1):
+        """
+        :param root_path:
+        :param batch_size:
+        """
+        if not os.path.exists(root_path):
+            logger.error("ILSVRC2012 dataset path not exist -> {}".format(root_path))
+            exit(-1)
+        self._batch_idx = 0
+        self._batch_size = batch_size
+        self._data_root_path = root_path
+        self._val_file = os.path.join(self._data_root_path, "..", "ILSVRC2015_val.txt")
+        if not os.path.exists(self._val_file):
+            logger.error("Not found val file -> {}".format(self._val_file))
+            exit(-1)
+        self._filepaths = list()
+        self._labels = list()
+        with open(self._val_file, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                filename, cls = line.strip().split()
+                _, ext = os.path.splitext(filename)
+                if ext not in [".jpg", ".JPEG", ".bmp", ".png", ".jpeg", ".BMP"]:
+                    continue
+                filepath = os.path.join(self._data_root_path, filename)
+                if not os.path.exists(filepath):
+                    continue
+                self._filepaths.append(filepath)
+                self._labels.append(int(cls))
+        self._total_num = len(self._filepaths)
