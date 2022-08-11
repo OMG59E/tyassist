d17b36ce5e98d8fca92206096de3d46e3795b927
邢卫国 <xing.weiguo@intellif.com>
2022-08-11 16:00:36 +0800

    :wrench: rename dpexec -> tyassist

--- examples/mxnet_squeezenet_v1_1/build.sh
+++ examples/mxnet_squeezenet_v1_1/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 /DEngine/tyassist/dpexec.py build -c config.yml --log_dir ./logs
+python3 /DEngine/tyassist/tyassist.py build -c config.yml --log_dir ./logs
--- examples/onnx_squeezenet_v1_1/test.sh
+++ examples/onnx_squeezenet_v1_1/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 /DEngine/tyassist/dpexec.py test -c config.yml --log_dir ./logs
+python3 /DEngine/tyassist/tyassist.py test -c config.yml --log_dir ./logs
