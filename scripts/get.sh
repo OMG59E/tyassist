cc9712dafb34b32afb2486a5ffe7323b45424b0f
pack <pack>
2022-11-28 20:40:17 +0800

    更新打包

--- scripts/get.sh
+++ scripts/get.sh
@@ -1,6 +1,11 @@
+set -e
+
 cd $(dirname $0)
 
-sh get_data.sh
-sh get_models.sh
+# get datasets
+wget ftp://192.168.33.100//date/ftp-intellif/DEngine/data/datasets --ftp-user=lijiao --ftp-password=lj --directory-prefix=$WORK_DIR -nH --cut-dirs=4 -r
 
-echo "download all tyexamples package files successfully."
+# get models
+wget ftp://192.168.33.100//date/ftp-intellif/DEngine/origin_models_dp2000/* --ftp-user=lijiao --ftp-password=lj --directory-prefix=$WORK_DIR/models -nH --cut-dirs=5 -r
+
+echo "download all tyassist package files successfully."
