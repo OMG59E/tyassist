cf62d84188974036b6d7bb308a3b49b06470e51c
pack <pack>
2022-12-01 18:59:50 +0800

    更新脚本

--- tyassist_build.sh
+++ tyassist_build.sh
@@ -7,7 +7,7 @@ else
 fi
 
 mkdir -p logs
-LOG_FILE="logs/tyassist_build_$(date "+%Y%m%d_%H%M%S").log"
+LOG_FILE="logs/tyassist_build_$1_$(date "+%Y%m%d_%H%M%S").log"
 
 echo "python3 $DENGINE_ROOT/tyassist/tyassist.py build $TARGET -c config.yml 2>&1 | tee $LOG_FILE"
 python3 $DENGINE_ROOT/tyassist/tyassist.py build $TARGET -c config.yml 2>&1 | tee $LOG_FILE
