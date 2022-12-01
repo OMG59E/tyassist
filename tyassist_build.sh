1155d47218ac08b60aa7d9f095f504009df0acdc
caoyan <cao.yan@intellif.com>
2022-12-01 16:34:31 +0800

    删除示例中的编译脚本，增加统一编译脚本

---
+++ tyassist_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$1" ]; then
+  TARGET="--target $1"
+else
+  echo "[warn] you didn't specify target in command, that will use the target specified in config file."
+fi
+
+python3 $DENGINE_ROOT/tyassist/tyassist.py build $TARGET -c config.yml --log_dir ./logs
