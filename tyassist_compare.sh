6115ab3b254ac2dbf1c98ec5f6227a617e5f83af
pack <pack>
2022-12-05 12:50:01 +0800

    脚本修改

--- tyassist_compare.sh
+++ tyassist_compare.sh
@@ -2,12 +2,15 @@
 
 if [ "$1" ]; then
   TARGET="--target $1"
+  NNP=$1
+elif [ "$NNP" ]; then
+  TARGET="--target $NNP"
 else
   echo "[warn] you didn't specify target in command, that will use the target specified in config file."
 fi
 
 mkdir -p logs
-LOG_FILE="logs/tyassist-compare-$1-$(date "+%Y-%m-%d-%H-%M-%S").log"
+LOG_FILE="logs/tyassist-compare-$NNP-$(date "+%Y-%m-%d-%H-%M-%S").log"
 
 echo "python3 $DENGINE_ROOT/tyassist/tyassist.py compare $TARGET -c config.yml 2>&1 | tee $LOG_FILE"
 python3 $DENGINE_ROOT/tyassist/tyassist.py compare $TARGET -c config.yml 2>&1 | tee $LOG_FILE
