8c9f5176e1c34e2ba833f20ad58865efbdb2086d
pack <pack>
2022-12-07 11:27:48 +0800

    增加data/pic到打包

--- scripts/get.sh
+++ scripts/get.sh
@@ -5,6 +5,9 @@ WORK_DIR=$(dirname $0)/..
 # get datasets
 wget ftp://192.168.33.100//date/ftp-intellif/DEngine/data/datasets --ftp-user=lijiao --ftp-password=lj --directory-prefix=$WORK_DIR -nH --cut-dirs=4 -r
 
+# get pic
+wget ftp://192.168.33.100//date/ftp-intellif/DEngine/data/pic --ftp-user=lijiao --ftp-password=lj --directory-prefix=$WORK_DIR -nH --cut-dirs=4 -r
+
 # get models
 wget ftp://192.168.33.100//date/ftp-intellif/DEngine/origin_models_dp2000/* --ftp-user=lijiao --ftp-password=lj --directory-prefix=$WORK_DIR/models -nH --cut-dirs=5 -r
 
