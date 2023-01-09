fe8095dbf2b0428aebf4d327a5943bb80243b459
caoyan <cao.yan@intellif.com>
2023-01-09 11:36:41 +0800

    修改onnx_squeezenet的收入尺寸为224，增加demo和test配置

---
+++ examples/onnx_squeezenet_v1_1/model_impl.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@File    : model_impl.py
+@Time    : 2022/7/21 下午2:02
+@Author  : xingwg
+@Email   : xing.weiguo@intellif.com
+@Software: PyCharm
+"""
+from base.classification import Classifier
+
+
+class SqueezeNetV1_1(Classifier):
+    pass
