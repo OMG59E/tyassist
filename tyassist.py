77201eb5156f84d831a299537c09f86258dfc251
pack <pack>
2023-02-09 14:41:29 +0800

    增加默认值说明

--- tyassist.py
+++ tyassist.py
@@ -375,11 +375,11 @@ if __name__ == "__main__":
                         choices=("nnp300", "nnp3020", "nnp310", "nnp320", "nnp400"),
                         help="Please specify a chip target")
     parser.add_argument("--dtype", "-t", type=str, default="int8", choices=("int8", "fp32"),
-                        help="Please specify one of them")
+                        help="Please specify one of them， default is int8")
     parser.add_argument("--backend", type=str, required=("demo" in sys.argv or "test" in sys.argv or "compare" in sys.argv),
                         choices=("chip", "iss", "tvm"), help="Please specify one of them")
     parser.add_argument("--log_dir", type=str, default="./logs",
-                        help="Please specify a log dir, default ./logs")
+                        help="Please specify a log dir, default is ./logs")
 
     args = parser.parse_args()
 
