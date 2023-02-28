3d5eaa382df9976a1274aef4e35b3bc647de1c61
pack <pack>
2023-02-28 19:51:22 +0800

    更新models目录

--- scripts/get.sh
+++ scripts/get.sh
@@ -9,6 +9,6 @@ wget ftp://192.168.33.100//date/ftp-intellif/DEngine/data/datasets --ftp-user=li
 wget ftp://192.168.33.100//date/ftp-intellif/DEngine/data/pic --ftp-user=lijiao --ftp-password=lj --directory-prefix=$WORK_DIR -nH --cut-dirs=4 -r
 
 # get models
-wget ftp://192.168.33.100//date/ftp-intellif/DEngine/models/* --ftp-user=lijiao --ftp-password=lj --directory-prefix=$WORK_DIR/models -nH --cut-dirs=5 -r
+wget ftp://192.168.33.100//date/ftp-intellif/DEngine/models/v1.0.5/* --ftp-user=lijiao --ftp-password=lj --directory-prefix=$WORK_DIR/models -nH --cut-dirs=6 -r
 
 echo "download all tyassist package files successfully."
