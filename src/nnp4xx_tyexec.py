0b578769d455cb8cb6298fa5470f79a8309b1647
xingwg <xing.weiguo@intellif.com>
2023-05-17 19:40:50 +0800

    :bug: 修复flops和cycles获取功能

--- src/nnp4xx_tyexec.py
+++ src/nnp4xx_tyexec.py
@@ -8,6 +8,7 @@
 """
 import torch
 import time
+import json
 import os
 from abc import ABC
 from utils import logger
@@ -316,14 +317,24 @@ class Nnp4xxTyExec(BaseTyExec, ABC):
         logger.warning("Nnp4xx not support compress analysis")
 
     def get_profile_info(self):
+        import tvm
         from tvm.contrib.edgex import estimate_FLOPs
         from tvm.contrib.edgex import estimate_cycles
-        # flops = estimate_FLOPs(self.relay_quant)
-        # cycles = estimate_cycles(self.relay_quant)
-        # with open(os.path.join(self.result_dir, "flops.json"), "w") as f:
-        #     f.write(json.dumps(flops, indent=2))
-        # with open(os.path.join(self.result_dir, "cycles.json"), "w") as f:
-        #     f.write(json.dumps(cycles, indent=2))
+        from tvm.contrib.edgex import build_config_nnp, optimize_nnp_model
+        flops = estimate_FLOPs(self.relay)
+        target_device = tvm.target.Target("edgex", host="edgex_virtual_host")
+        with build_config_nnp():
+            optimized_mod, optimized_params = optimize_nnp_model(
+                self.relay_quant,
+                self.params_quant,
+                target_device=target_device,
+                keep_params=True,
+            )
+        cycles = estimate_cycles(optimized_mod)
+        with open(os.path.join(self.result_dir, "flops.json"), "w") as f:
+            f.write(json.dumps(flops, indent=2))
+        with open(os.path.join(self.result_dir, "cycles.json"), "w") as f:
+            f.write(json.dumps(cycles, indent=2))
 
     @staticmethod
     def save_relay_to_model(quant_model_path, relay_func, params):
