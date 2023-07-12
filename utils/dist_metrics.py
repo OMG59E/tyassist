06d9272ec5ce5ca7d3d5ba191f398d35342d9626
xingwg <xing.weiguo@intellif.com>
2023-07-12 10:56:07 +0800

    :zap: add numerical_distance

--- utils/dist_metrics.py
+++ utils/dist_metrics.py
@@ -9,19 +9,25 @@
 import numpy as np
 
 
-def cosine_distance(data1, data2):
+def cosine_distance(v1, v2):
     """余弦距离
-    :param data1:
-    :param data2:
-    :return:
     """
-    v1_d = data1.flatten().astype("float64")
-    v2_d = data2.flatten().astype("float64")
+    v1_d = v1.flatten().astype("float64")
+    v2_d = v2.flatten().astype("float64")
     assert len(v1_d) == len(v2_d), "v1 dim must be == v2 dim"
     v1_d[v1_d == np.inf] = np.finfo(np.float16).max
     v2_d[v2_d == np.inf] = np.finfo(np.float16).max
     v1_d[v1_d == -np.inf] = np.finfo(np.float16).min
     v2_d[v2_d == -np.inf] = np.finfo(np.float16).min
-    # v1_norm = v1_d / np.linalg.norm(v1_d)
-    # v2_norm = v2_d / np.linalg.norm(v2_d)
     return v1_d.dot(v2_d) / np.maximum(np.linalg.norm(v1_d) * np.linalg.norm(v2_d), np.finfo(np.float32).eps)
+
+
+def numerical_distance(v1, v2, epsilon=1e-3):
+    v1_d = v1.flatten().astype("float64")
+    v2_d = v2.flatten().astype("float64")
+    assert len(v1_d) == len(v2_d), "v1 dim must be == v2 dim"
+    v1_d[v1_d == np.inf] = np.finfo(np.float16).max
+    v2_d[v2_d == np.inf] = np.finfo(np.float16).max
+    v1_d[v1_d == -np.inf] = np.finfo(np.float16).min
+    v2_d[v2_d == -np.inf] = np.finfo(np.float16).min
+    return len(np.where(np.abs(v1_d - v2_d) < epsilon)) / len(v2_d)
