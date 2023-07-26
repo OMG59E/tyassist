66798bf1b54aed67814674ba2a24a72ec38630d5
xingwg <xing.weiguo@intellif.com>
2023-07-26 20:15:24 +0800

    :bug: fixed tensorflow_imgD

--- examples/tensorflow_imgD/custom_preprocess.py
+++ examples/tensorflow_imgD/custom_preprocess.py
@@ -70,13 +70,14 @@ class CustomTensorFlowImgD(BaseCustomPreprocess):
         out = np.expand_dims(out.transpose((2, 0, 1)), axis=0).astype("float32")
         return out
 
-    def get_single_data(self, filepath, idx):
+    def get_single_data(self, filepaths, idx):
         """用于处理指定输入图片的预处理，一般用于推理计算相似度
             不需要norm归一化，工具链会根据配置文件，内部进行norm
-        :param filepath:
+        :param filepaths:
         :param idx:  不支持输入的索引
         :return:
         """
+        filepath = filepaths[0]
         not_ext_path, ext = os.path.splitext(filepath)
         name = not_ext_path.split("/")[-1]
         depth_path = os.path.join(os.path.dirname(filepath), "..", "depth", "{}_depth{}".format(name, ext))
