eb62f3d834ba7a3c13cebc55f377c23b88f3eb08
xingwg <xing.weiguo@intellif.com>
2023-08-23 15:06:39 +0800

    :art: update yolov5s_custom_3xx model_impl.py

--- examples/onnx_yolov5s_custom/model_impl.py
+++ examples/onnx_yolov5s_custom/model_impl.py
@@ -7,6 +7,7 @@
 @Software: PyCharm
 """
 from base.detection import Detector
+from utils.postprocess import scale_coords
 
 
 class YoloV5s(Detector):
@@ -14,3 +15,13 @@ class YoloV5s(Detector):
         super().__init__(**kwargs)
         self._iou_threshold = 0.45
         self._conf_threshold = 0.25
+
+    def _postprocess(self, outputs, cv_images=None):
+        outputs = outputs[0]
+        detections = list()
+        for idx, cv_image in enumerate(cv_images):
+            output = outputs[idx]
+            output = output[output[:, 4] > 0]
+            output[:, :4] = scale_coords(self._input_size, output[:, :4], cv_image.shape).round()
+            detections.append(output)
+        return detections
